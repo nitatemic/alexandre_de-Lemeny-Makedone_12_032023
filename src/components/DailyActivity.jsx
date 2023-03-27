f85352e33d1094bb9ca362b2baf5037ce4b0050a
nitatemic <alexandre@nitatemic.dev>
2023-03-27 20:11:08 +0200

    It starts to become responsive

--- src/components/DailyActivity.jsx
+++ src/components/DailyActivity.jsx
@@ -40,28 +40,32 @@ export default function DailyActivity(props) {
 
   return (
     <div className="dailyActivity">
-      <h2 className="dailyActivity-title">Activité quotidienne</h2>
-      <ResponsiveContainer width="100%" height={300}>
-        <BarChart
-          data={sportData}
-          className="dailyActivity-chart"
-          margin={{
-            top: 0,
-            right: 0,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3" vertical={false} />
-          <XAxis dataKey="Jour" tickLine={false} tickMargin={10} />
-          <YAxis orientation="right" allowDecimals={false} domain={['dataMin - 3', 'dataMax + 2']} dataKey="Poids" yAxisId="YAxisPoids" tickLine={false} axisLine={false} tickMargin={10} />
-          <YAxis orientation="left" allowDecimals={false} domain={['dataMin - 50', 'dataMax + 30']} dataKey="Calories" yAxisId="YAxisCalories" hide />
-          <Tooltip />
-          <Legend align="right" verticalAlign="top" iconSize={8} wrapperStyle={{ paddingBottom: 30 }} />
-          <Bar dataKey="Poids" name="Poids (kg)" legendType="circle" unit="kg" fill="#282D30" yAxisId="YAxisPoids" radius={[10, 10, 0, 0]} barSize={7} />
-          <Bar dataKey="Calories" name="Calories brûlées (kCal)" legendType="circle" unit="kCal" fill="#E60000" yAxisId="YAxisCalories" radius={[10, 10, 0, 0]} barSize={7} />
-        </BarChart>
-      </ResponsiveContainer>
+      <div className="dailyActivity-container">
+        <h2 className="dailyActivity-title">Activité quotidienne</h2>
+        <div className="dailyActivity-graph">
+          <ResponsiveContainer width="100%" height="100%">
+            <BarChart
+              data={sportData}
+              className="dailyActivity-chart"
+              margin={{
+                top: 0,
+                right: 0,
+                left: 0,
+                bottom: 0,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3" vertical={false} />
+              <XAxis dataKey="Jour" tickLine={false} tickMargin={10} />
+              <YAxis orientation="right" allowDecimals={false} domain={['dataMin - 3', 'dataMax + 2']} dataKey="Poids" yAxisId="YAxisPoids" tickLine={false} axisLine={false} tickMargin={10} />
+              <YAxis orientation="left" allowDecimals={false} domain={['dataMin - 50', 'dataMax + 30']} dataKey="Calories" yAxisId="YAxisCalories" hide />
+              <Tooltip />
+              <Legend align="right" verticalAlign="top" iconSize={8} wrapperStyle={{ paddingBottom: 30 }} />
+              <Bar dataKey="Poids" name="Poids (kg)" legendType="circle" unit="kg" fill="#282D30" yAxisId="YAxisPoids" radius={[10, 10, 0, 0]} barSize={7} />
+              <Bar dataKey="Calories" name="Calories brûlées (kCal)" legendType="circle" unit="kCal" fill="#E60000" yAxisId="YAxisCalories" radius={[10, 10, 0, 0]} barSize={7} />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
     </div>
   );
 }
