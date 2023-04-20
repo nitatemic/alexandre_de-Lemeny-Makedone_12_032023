d4469888045483d6f6a107e87e828f652ea18cdc
nitatemic <alexandre@nitatemic.dev>
2023-04-20 19:03:39 +0200

    Fix some warnings

--- src/services/user.js
+++ src/services/user.js
@@ -1,32 +1,26 @@
-import { API } from './variables.js';
-import requests from '../requests/requests.js';
+import requests from '../requests/requests';
 
 /**
  * This is an asynchronous function that retrieves user data from an API endpoint,
  * with different URLs depending on the environment.
- * @param id - The `id` parameter is a unique identifier for the user whose data is being requested.
+ * @param userID - The `id` parameter is a unique identifier for the user whose data is being requested.
  * It is used to construct the URL for the API endpoint that will be called to retrieve the user data.
  * @returns the `data` property of the JSON response obtained from the API endpoint.
  */
-export default async function getUser(id) {
-  let response;
-  if (process.env.NODE_ENV === 'production') {
-    response = await fetch(`${API}user/${id}`);
-  } else {
-    response = await fetch(`${API}user/${id}.json`);
-  }
+export default async function getUser(userID) {
+  const response = await requests(userID, 'user');
   const data = await response.json();
   return data.data;
 }
 
 /**
- * @param id - The parameter `id` is a unique identifier for a user,
+ * @param userID - The parameter `id` is a unique identifier for a user,
  * which is used to fetch their data from an API.
  * @returns the `score` property of the `data` object obtained from the API response for the user
  * with the specified `id`.
  */
 export async function getUserGoalCompletion(userID) {
- const response = await requests(userID, 'user');
+  const response = await requests(userID, 'user');
   const data = await response.json();
   const score = data.data;
   const result = score.score;
@@ -34,7 +28,7 @@ export async function getUserGoalCompletion(userID) {
   const goalValue = result * 100;
   const RemainingValue = 100 - goalValue;
 
-  const table = [
+  return [
     {
       name: 'Goal',
       value: goalValue,
@@ -44,5 +38,4 @@ export async function getUserGoalCompletion(userID) {
       value: RemainingValue,
     },
   ];
-  return table;
 }
